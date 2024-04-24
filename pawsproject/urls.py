e2389abe727656555c13bf4a05905e31d4b3a9cb
Dale Dear <kristendaledear@gmail.com>
2024-04-24 10:19:58 -0500

    initial commit

--- pawsproject/urls.py
+++ pawsproject/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from pawsapi.views import register_user, login_user
 
 router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
+    path("register", register_user),  # Enables http://localhost:8000/register
+    path("login", login_user),  # Enables http://localhost:8000/login
 ]
-
